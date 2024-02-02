dc8510d3690563ec03b237871149eb946b51b5d9
midCenturyDude7 <mgriffes@gmail.com>
2024-02-01 19:13:19 -0800

    Set up route for first project

--- app/page.js
+++ app/page.js
@@ -1,10 +1,15 @@
-import { Herr_Von_Muellerhoff } from "next/font/google";
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center">
-      Hello, World!
+    <div className="flex min-h-screen items-center justify-center">
+      <ul className="flex">
+        <Link href="/1-step">
+          <li>1-steps</li>
+        </Link>
+        <li className="pl-4">2-email</li>
+        <li className="pl-4">3-header-part-1</li>
+      </ul>
     </div>
   );
 }
