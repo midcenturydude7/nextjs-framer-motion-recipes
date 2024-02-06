2dd06dd368894ef2924233f009318b8d71f6b150
midCenturyDude7 <mgriffes@gmail.com>
2024-02-05 22:17:15 -0800

    Set up carousel project

--- app/4-carousel/page.js
+++ app/4-carousel/page.js
@@ -1,6 +1,51 @@
 "use client";
+import Image from "next/image";
 import React from "react";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/outline";
 
-export default function page() {
-  return <div>Carousel Project</div>;
+let images = [
+  "/images/1.jpeg",
+  "/images/2.jpeg",
+  "/images/3.jpeg",
+  "/images/4.jpeg",
+  "/images/5.jpeg",
+  "/images/6.jpeg",
+];
+
+export default function Page() {
+  let [index, setIndex] = React.useState(0);
+
+  return (
+    <div className="h-full bg-black">
+      <div className="mx-auto flex h-full max-w-7xl flex-col justify-center">
+        <div className="relative">
+          <Image
+            alt="array of nyc images"
+            src={images[1]}
+            width={300}
+            height={300}
+            className="aspect-[3/2] object-cover"
+          />
+
+          {index > 0 && (
+            <button
+              className="absolute left-2 top-1/2 -mt-4 flex h-8 w-8 items-center justify-center rounded-full bg-white/60 transition hover:bg-white/80"
+              onClick={() => setIndex(index - 1)}
+            >
+              <ChevronLeftIcon className="h-6 w-6" />
+            </button>
+          )}
+
+          {index + 1 < images.length && (
+            <button
+              className="absolute right-2 top-1/2 -mt-4 flex h-8 w-8 items-center justify-center rounded-full bg-white/60 transition hover:bg-white/80"
+              onClick={() => setIndex(index + 1)}
+            >
+              <ChevronRightIcon className="h-6 w-6" />
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
