e6f7eff6b86c07e50908ce18c90ad9e8be06dd08
midCenturyDude7 <mgriffes@gmail.com>
2024-02-07 21:14:17 -0800

    Image carousel with framer-motion

--- app/4-carousel/page.js
+++ app/4-carousel/page.js
@@ -1,6 +1,7 @@
 "use client";
 import Image from "next/image";
 import React from "react";
+import { motion, AnimatePresence, MotionConfig } from "framer-motion";
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/outline";
 
 let images = [
@@ -16,36 +17,53 @@ export default function Page() {
   let [index, setIndex] = React.useState(0);
 
   return (
-    <div className="h-full bg-black">
-      <div className="mx-auto flex h-full max-w-7xl flex-col justify-center items-center">
-        <div className="relative h-3/6 w-4/6">
-          <Image
-            alt="array of nyc images"
-            src={images[index]}
-            objectFit="cover"
-            fill={true}
-            className="aspect-[3/2] object-cover"
-          />
-
-          {index > 0 && (
-            <button
-              className="absolute left-2 top-1/2 -mt-4 flex h-8 w-8 items-center justify-center rounded-full bg-white/60 transition hover:bg-white/80"
-              onClick={() => setIndex(index - 1)}
-            >
-              <ChevronLeftIcon className="h-6 w-6" />
-            </button>
-          )}
-
-          {index + 1 < images.length && (
-            <button
-              className="absolute right-2 top-1/2 -mt-4 flex h-8 w-8 items-center justify-center rounded-full bg-white/60 transition hover:bg-white/80"
-              onClick={() => setIndex(index + 1)}
-            >
-              <ChevronRightIcon className="h-6 w-6" />
-            </button>
-          )}
+    <MotionConfig transition={{ duration: 1.7, ease: [0.32, 0.72, 0, 1] }}>
+      <div className="h-full bg-black">
+        <div className="mx-auto flex h-full max-w-7xl flex-col items-center justify-center">
+          <div className="relative overflow-hidden">
+            <motion.div animate={{ x: `-${index * 100}%` }} className="flex">
+              {images.map((image) => (
+                <Image
+                  key={image}
+                  alt="array of nyc images"
+                  src={image}
+                  width={1280}
+                  height={500}
+                  className="aspect-[3/2] object-cover"
+                />
+              ))}
+            </motion.div>
+            <AnimatePresence initial={false}>
+              {index > 0 && (
+                <motion.button
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 0.7 }}
+                  exit={{ opacity: 0, pointerEvents: "none" }}
+                  whileHover={{ opacity: 1 }}
+                  className="absolute left-2 top-1/2 -mt-4 flex h-8 w-8 items-center justify-center rounded-full bg-white text-black"
+                  onClick={() => setIndex(index - 1)}
+                >
+                  <ChevronLeftIcon className="h-6 w-6" />
+                </motion.button>
+              )}
+            </AnimatePresence>
+            <AnimatePresence initial={false}>
+              {index + 1 < images.length && (
+                <motion.button
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 0.7 }}
+                  exit={{ opacity: 0, pointerEvents: "none" }}
+                  whileHover={{ opacity: 1 }}
+                  className="bg-white/ absolute right-2 top-1/2 -mt-4 flex h-8 w-8 items-center justify-center rounded-full bg-white text-black"
+                  onClick={() => setIndex(index + 1)}
+                >
+                  <ChevronRightIcon className="h-6 w-6" />
+                </motion.button>
+              )}
+            </AnimatePresence>
+          </div>
         </div>
       </div>
-    </div>
+    </MotionConfig>
   );
 }
