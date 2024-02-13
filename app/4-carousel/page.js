fd891a8c549651d48c3ce4c6a6e552c5c94da4de
midCenturyDude7 <mgriffes@gmail.com>
2024-02-12 22:27:51 -0800

    Added useKeypress hook for right and left arrow navigation

--- app/4-carousel/page.js
+++ app/4-carousel/page.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import React from "react";
 import { motion, AnimatePresence, MotionConfig } from "framer-motion";
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/outline";
+import useKeypress from "react-use-keypress";
 
 let images = [
   "/images/1.jpeg",
@@ -11,6 +12,12 @@ let images = [
   "/images/4.jpeg",
   "/images/5.jpeg",
   "/images/6.jpeg",
+  "/images/1.jpeg?1",
+  "/images/2.jpeg?1",
+  "/images/3.jpeg?1",
+  "/images/4.jpeg?1",
+  "/images/5.jpeg?1",
+  "/images/6.jpeg?1",
 ];
 
 let collapsedAspectRatio = 1 / 3;
@@ -20,6 +27,14 @@ let gap = 3.25;
 
 export default function Page() {
   let [index, setIndex] = React.useState(0);
+
+  useKeypress("ArrowRight", () => {
+    if (index < images.length - 1) setIndex(index + 1);
+  });
+
+  useKeypress("ArrowLeft", () => {
+    if (index > 0) setIndex(index - 1);
+  });
 
   return (
     <MotionConfig transition={{ duration: 1.7, ease: [0.32, 0.72, 0, 1] }}>
