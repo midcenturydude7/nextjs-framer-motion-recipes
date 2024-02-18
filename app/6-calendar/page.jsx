3bc2c268751024d05b3070083c03ac30f1be9a92
midCenturyDude7 <mgriffes@gmail.com>
2024-02-17 20:55:57 -0800

    Created ResizablePanel component to handle six weeks resizing

--- app/6-calendar/page.jsx
+++ app/6-calendar/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React from "react";
+import ResizablePanel from "../components/ResizablePanel";
 import { motion, AnimatePresence, MotionConfig } from "framer-motion";
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/outline";
 import {
@@ -21,16 +22,22 @@ export default function Page() {
     format(new Date(), "yyyy-MM"),
   );
   let [direction, setDirection] = React.useState();
+  let [isAnimating, setIsAnimating] = React.useState(false);
+
   let month = parse(monthString, "yyyy-MM", new Date());
 
   function nextMonth() {
+    if (isAnimating) return;
+
     let next = addMonths(month, 1);
 
     setMonthString(format(next, "yyyy-MM"));
     setDirection(1);
+    setIsAnimating(true);
   }
 
   function previousMonth() {
+    if (isAnimating) return;
     let previous = subMonths(month, 1);
 
     setMonthString(format(previous, "yyyy-MM"));
@@ -43,72 +50,83 @@ export default function Page() {
   });
 
   return (
-    <MotionConfig transition={{ duration: 0.5 }}>
+    <MotionConfig transition={{ duration: 0.25 }}>
       <div className="flex min-h-screen items-start bg-stone-800 pt-16 text-stone-900">
         <div className="relative mx-auto w-full max-w-md overflow-hidden rounded-2xl bg-white">
           <div className="py-8">
             <div className="flex flex-col justify-center rounded text-center">
-              <AnimatePresence
-                mode="popLayout"
-                initial={false}
-                custom={direction}
-              >
-                <motion.div
-                  key={monthString}
-                  initial="enter"
-                  animate="middle"
-                  exit="exit"
+              <ResizablePanel>
+                <AnimatePresence
+                  mode="popLayout"
+                  initial={false}
+                  custom={direction}
+                  onExitComplete={() => setIsAnimating(false)}
                 >
-                  <header className="relative flex justify-between px-8">
-                    <motion.button
+                  <motion.div
+                    key={monthString}
+                    initial="enter"
+                    animate="middle"
+                    exit="exit"
+                  >
+                    <header className="relative flex justify-between px-8">
+                      <motion.button
+                        variants={removeImmediately}
+                        className="z-10 rounded-full p-1.5 hover:bg-stone-100"
+                        onClick={previousMonth}
+                      >
+                        <ChevronLeftIcon className="h-4 w-4" />
+                      </motion.button>
+                      <motion.p
+                        variants={variants}
+                        custom={direction}
+                        className="absolute inset-0 flex items-center justify-center font-semibold"
+                      >
+                        {format(month, "MMMM yyyy")}
+                      </motion.p>
+                      <motion.button
+                        variants={removeImmediately}
+                        className="z-10 rounded-full p-1.5 hover:bg-stone-100"
+                        onClick={nextMonth}
+                      >
+                        <ChevronRightIcon className="h-4 w-4" />
+                      </motion.button>
+                      <div
+                        className="absolute inset-0"
+                        style={{
+                          backgroundImage:
+                            "linear-gradient(to right, white 10%, transparent 30%, transparent 70%, white 90%",
+                        }}
+                      />
+                    </header>
+                    <motion.div
                       variants={removeImmediately}
-                      className="z-10 rounded-full p-1.5 hover:bg-stone-100"
-                      onClick={previousMonth}
+                      className="mt-6 grid grid-cols-7 gap-y-6 px-8"
                     >
-                      <ChevronLeftIcon className="h-4 w-4" />
-                    </motion.button>
-                    <motion.p
+                      <span className="font-medium text-stone-500">Su</span>
+                      <span className="font-medium text-stone-500">Mo</span>
+                      <span className="font-medium text-stone-500">Tu</span>
+                      <span className="font-medium text-stone-500">We</span>
+                      <span className="font-medium text-stone-500">Th</span>
+                      <span className="font-medium text-stone-500">Fr</span>
+                      <span className="font-medium text-stone-500">Sa</span>
+                    </motion.div>
+                    <motion.div
                       variants={variants}
                       custom={direction}
-                      className="absolute inset-0 flex items-center justify-center font-semibold"
+                      className="mt-6 grid grid-cols-7 gap-y-6 px-8"
                     >
-                      {format(month, "MMMM yyyy")}
-                    </motion.p>
-                    <button
-                      className="z-10 rounded-full p-1.5 hover:bg-stone-100"
-                      onClick={nextMonth}
-                    >
-                      <ChevronRightIcon className="h-4 w-4" />
-                    </button>
-                  </header>
-                  <motion.div
-                    variants={removeImmediately}
-                    className="mt-6 grid grid-cols-7 gap-y-6 px-8"
-                  >
-                    <span className="font-medium text-stone-500">Su</span>
-                    <span className="font-medium text-stone-500">Mo</span>
-                    <span className="font-medium text-stone-500">Tu</span>
-                    <span className="font-medium text-stone-500">We</span>
-                    <span className="font-medium text-stone-500">Th</span>
-                    <span className="font-medium text-stone-500">Fr</span>
-                    <span className="font-medium text-stone-500">Sa</span>
+                      {days.map((day) => (
+                        <span
+                          className={`${isSameMonth(day, month) ? "" : "text-stone-300"}`}
+                          key={format(day, "yyyy-MM-dd")}
+                        >
+                          {format(day, "d")}
+                        </span>
+                      ))}
+                    </motion.div>
                   </motion.div>
-                  <motion.div
-                    variants={variants}
-                    custom={direction}
-                    className="mt-6 grid grid-cols-7 gap-y-6 px-8"
-                  >
-                    {days.map((day) => (
-                      <span
-                        className={`${isSameMonth(day, month) ? "" : "text-stone-300"}`}
-                        key={format(day, "yyyy-MM-dd")}
-                      >
-                        {format(day, "d")}
-                      </span>
-                    ))}
-                  </motion.div>
-                </motion.div>
-              </AnimatePresence>
+                </AnimatePresence>
+              </ResizablePanel>
             </div>
           </div>
         </div>
@@ -119,11 +137,11 @@ export default function Page() {
 
 let variants = {
   enter: (direction) => {
-    return { x: `${-100 * direction}%` };
+    return { x: `${-100 * direction}%`, opacity: 0 };
   },
-  middle: { x: "0%" },
+  middle: { x: "0%", opacity: 1 },
   exit: (direction) => {
-    return { x: `${100 * direction}%` };
+    return { x: `${100 * direction}%`, opacity: 0 };
   },
 };
 
