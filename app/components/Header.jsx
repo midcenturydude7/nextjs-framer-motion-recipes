45177c298bc920ea87dfc6690f59ea49c1bbd2fd
midCenturyDude7 <mgriffes@gmail.com>
2024-02-24 20:29:16 -0800

    Styling the menu items

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -4,24 +4,36 @@ export default function Header() {
   return (
     <div className="fixed inset-x-0 z-10 bg-gray-900 shadow-lg">
       <ul className="flex items-center justify-center gap-3 py-10 text-gray-300">
-        <li className="pr-2">Recipes:</li>
+        <li className="rounded-lg bg-slate-800 px-3 py-1">Recipes:</li>
         <Link href="/1-step">
-          <li>1-steps</li>
+          <li className="min-w-24 rounded-lg bg-slate-700 px-3 py-1 text-center">
+            1-steps
+          </li>
         </Link>
         <Link href="/2-email">
-          <li className="pl-4">2-email</li>
+          <li className="min-w-24 rounded-lg bg-slate-700 px-3 py-1 text-center">
+            2-email
+          </li>
         </Link>
         <Link href="/3-header">
-          <li className="pl-4">3-header</li>
+          <li className="min-w-24 rounded-lg bg-slate-700 px-3 py-1 text-center">
+            3-header
+          </li>
         </Link>
         <Link href="/4-carousel">
-          <li className="pl-4">4-carousel</li>
+          <li className="min-w-24 rounded-lg bg-slate-700 px-3 py-1 text-center">
+            4-carousel
+          </li>
         </Link>
         <Link href="/5-panel">
-          <li className="pl-4">5-panel</li>
+          <li className="min-w-24 rounded-lg bg-slate-700 px-3 py-1 text-center">
+            5-panel
+          </li>
         </Link>
         <Link href="/6-calendar">
-          <li className="pl-4">6-calendar</li>
+          <li className="min-w-24 rounded-lg bg-slate-700 px-3 py-1 text-center">
+            6-calendar
+          </li>
         </Link>
       </ul>
     </div>
